0bb747e62db2a910f7129d9927abcd3d752a6539
Kushal B G <kushalbg06@gmail.com>
2025-11-25 20:17:29 +0530

    update schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,34 +1,62 @@
 from pydantic import BaseModel
+from typing import Optional,List
+from datetime import datetime
 
-class productcreate(BaseModel):
-    name:str
-    price:int
-    discription:str
-    is_stock:bool=True
-
-class productout(BaseModel):
-    id:int
-    name:str
-    price:int
-    discription:str
-    is_stock:bool
-
-    class Config:
-        from_attributes = True
-class user(BaseModel):
+class UserCreate(BaseModel):
     name:str
     email:str
     password:str
 
-class showuser(BaseModel):
-     id:int
-     name:str
-     email:str
-     password:str
+class UserResponse(BaseModel):
+    id:int
+    name:str
+    email:str
 
-     class Config:
-         from_attributes=True
+    class Config:
+        orm_mode = True
 
-class login(BaseModel):
-    username:str
-    password:str
+class ProductCreate(BaseModel):
+    name:str
+    price:float
+    stock:int
+    discription:str
+
+class ProductResponse(BaseModel):
+    id:int
+    name:str
+    price:float
+    stock:int
+    discription:Optional[str]
+
+    class Config:
+        orm_mode = True
+
+class CratCreate(BaseModel):
+    product_id:int
+    quantity:int
+
+class CartResponse(BaseModel):
+    id:int
+    user_id:int
+    product_id:int
+    quantity:int
+
+    class Config:
+        orm_mode = True
+
+class OrderItemResponse(BaseModel):
+    id:int
+    product_id:int
+    quantity:int
+    price:float
+
+    class Config:
+        orm_mode = True
+class OrderResponse(BaseModel):
+    id:int
+    total_amount=float
+    create_at=datetime
+    items:List[OrderItemResponse]
+
+    class Config:
+        orm_mode = True   
