e465c18350ab7e53dda7a32423ee70e4839020b8
Kushal B G <kushalbg06@gmail.com>
2025-11-27 19:07:32 +0530

    update cart router

--- app/routers/cart_router.py
+++ app/routers/cart_router.py
@@ -2,15 +2,16 @@ from fastapi import APIRouter,Depends,HTTPException,status
 from sqlalchemy.orm import session
 from ..database import get_db
 from .. import models,schemas,database
+from typing import List
 
 router=APIRouter(
    prefix="/cart",
    tags=["Carts"] 
 )
 
-@router.post('/',response_model=schemas.CartResponse,status_code=status.HTTP_201_CREATED)
+@router.post('/add',response_model=schemas.CartResponse,status_code=status.HTTP_201_CREATED)
 def additem_cart(item:schemas.CratCreate,db:session=Depends(get_db)):
-    new_item=db.query(models.Cart).filter(models.Cart.id==item.product_id).first()
+    new_item=db.query(models.Product).filter(models.Product.id==item.product_id).first()
     if not new_item:
         raise HTTPException(404,detail="product not found")
     new_item=models.Cart(
@@ -23,3 +24,16 @@ def additem_cart(item:schemas.CratCreate,db:session=Depends(get_db)):
     db.refresh(new_item)
     return new_item
 
+@router.get("/",response_model=List[schemas.CartResponse],status_code=status.HTTP_200_OK)
+def get_all(db:session=Depends(get_db)):
+    items=db.query(models.Cart).filter(models.Cart.user_id==1).all()
+    return items
+
+@router.delete("/{id}",status_code=status.HTTP_200_OK)
+def delete_items(id:int,db:session=Depends(get_db)):
+    cart_items=db.query(models.Cart).filter(models.Cart.id==id).first()
+    if not cart_items:
+        raise HTTPException(404,detail="cart items not found")
+    db.delete(cart_items)
+    db.commit()
+    return {"message":"cart item is Removed Succesfully"}
